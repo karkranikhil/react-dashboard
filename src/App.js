6dcd396558137eb8fb7f98867c140e7b7b802f48
karkran <nikhil.karkra@anz.com>
2018-10-28 18:33:05 +1100

    page layout with state and basic react

--- src/App.js
+++ src/App.js
@@ -1,32 +1,52 @@
 import React, { Component } from 'react';
 import './App.css';
-import styled from 'styled-components'
+import styled, {css} from 'styled-components'
 
 const Logo = styled.div`
   font-size:1.5em;
 `
 const ControlButton = styled.div`
+  cursor: pointer;
+  line-height: 30px;
+${props=>props.active && css`
+  text-shadow: 0px 0px 60px #03ff03;
+`}
 `
-const AppLayout = styled.div`
-  padding:40px;
+const NavBar = styled.div`
+  margin-bottom:40px;
   display:grid;
   grid-template-columns:180px auto 100px 100px;
 `
+const Content = styled.div``
+const AppLayout = styled.div`
+padding:40px;
+`
 
 class App extends Component {
+
+  state={
+    page:'dashboard'
+  }
+  displayingDashboard = () =>this.state.page === 'dashboard'
+  displayingSettings = () =>this.state.page === 'settings'
   render() {
     return (
       <AppLayout>
-      <Logo>
-        CryptoDash
-      </Logo>
-      <div></div>
-      <ControlButton >
-        Dashboard
-      </ControlButton>
-      <ControlButton >
-        Setting
-      </ControlButton>
+        <NavBar>
+          <Logo>
+            CryptoDash
+          </Logo>
+          <div></div>
+          <ControlButton onClick={()=>{this.setState({page:'dashboard'})}} active={this.displayingDashboard()}>
+            Dashboard
+          </ControlButton>
+          <ControlButton onClick={()=>{this.setState({page:'settings'})}} active={this.displayingSettings()}>
+            Setting
+          </ControlButton>
+        </NavBar>
+      <Content>
+        hello i am {this.state.page}
+      </Content>
     </AppLayout>
     );
   }
