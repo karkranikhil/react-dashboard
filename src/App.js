c82d9417b8f9e7045b83b082bf76ede08335079e
karkran <nikhil.karkra@anz.com>
2018-10-28 18:00:36 +1100

    CSS Grid basic setup

--- src/App.js
+++ src/App.js
@@ -2,22 +2,32 @@ import React, { Component } from 'react';
 import './App.css';
 import styled from 'styled-components'
 
-const CustomElement = styled.div`
-  font-size:30px;
+const Logo = styled.div`
+  font-size:1.5em;
 `
-const BlueElement = styled(CustomElement)`
+const ControlButton = styled.div`
 `
+const AppLayout = styled.div`
+  padding:40px;
+  display:grid;
+  grid-template-columns:180px auto 100px 100px;
+`
+
 class App extends Component {
   render() {
     return (
-      <div>
-      <CustomElement>
-        hello
-      </CustomElement>
-      <BlueElement>
-      hello
-    </BlueElement>
-    </div>
+      <AppLayout>
+      <Logo>
+        CryptoDash
+      </Logo>
+      <div></div>
+      <ControlButton >
+        Dashboard
+      </ControlButton>
+      <ControlButton >
+        Setting
+      </ControlButton>
+    </AppLayout>
     );
   }
 }
