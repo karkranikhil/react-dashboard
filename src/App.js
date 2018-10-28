9c1ef9235d1e8a69a467af6199086f29f8f6921c
karkran <nikhil.karkra@anz.com>
2018-10-28 21:01:07 +1100

    create Navbar and moving the code from App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
-import styled, {css} from 'styled-components'
-
-const Logo = styled.div`
-  font-size:1.5em;
-`
-const ControlButton = styled.div`
-  cursor: pointer;
-  line-height: 30px;
-${props=>props.active && css`
-  text-shadow: 0px 0px 60px #03ff03;
-`}
-`
-const NavBar = styled.div`
-  margin-bottom:40px;
-  display:grid;
-  grid-template-columns:180px auto 100px 100px;
-`
+import styled from 'styled-components'
+import NavBar from './NavBar'
 const Content = styled.div``
 const AppLayout = styled.div`
 padding:40px;
@@ -62,18 +47,7 @@ class App extends Component {
   render() {
     return (
       <AppLayout>
-        <NavBar>
-          <Logo>
-            CryptoDash
-          </Logo>
-          <div></div>
-          {!this.state.firstVisit && <ControlButton onClick={()=>{this.setState({page:'dashboard'})}} active={this.displayingDashboard()}>
-            Dashboard
-          </ControlButton>}
-          <ControlButton onClick={()=>{this.setState({page:'settings'})}} active={this.displayingSettings()}>
-            Setting
-          </ControlButton>
-        </NavBar>
+        {NavBar.call(this)}
       <Content>
         {this.displayingSettings() && this.settingsContent()}
       </Content>
