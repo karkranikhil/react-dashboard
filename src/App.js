4a78ca84d7881f47dd1e24734e85ad08186a98e0
karkran <nikhil.karkra@anz.com>
2018-10-28 17:32:54 +1100

    some changes for styled components

--- src/App.js
+++ src/App.js
@@ -4,13 +4,22 @@ import styled from 'styled-components'
 
 const CustomElement = styled.div`
   color:green;
+  font-size:30px;
+`
+const BlueElement = styled(CustomElement)`
+  color:blue;
 `
 class App extends Component {
   render() {
     return (
+      <div>
       <CustomElement>
         hello
       </CustomElement>
+      <BlueElement>
+      hello
+    </BlueElement>
+    </div>
     );
   }
 }
