0f47ad85533f41c44898b14accea87062c881432
karkran <nikhil.karkra@anz.com>
2018-10-28 17:28:24 +1100

    set up styled components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components'
 
+const CustomElement = styled.div`
+  color:green;
+`
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Crypto Dashboard</h1>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <CustomElement>
+        hello
+      </CustomElement>
     );
   }
 }
